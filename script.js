fb2b705cff5a6ce0e5f684f1ee9cffe70d30f761
Joe <97924558+myspace8@users.noreply.github.com>
2022-11-28 21:12:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+/* A simple guess the number type game. It should choose a random number between 1 and 100, then challenge the player
+...
+*/ 
+
+// Adding variables to store out data
+
+
+// Generating random number
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+// Functions
+function checkGuess() {
+    const userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
+    }
+    guesses.textContent += userGuess + ' ';
+    guessField.value = '';
+    guessField.focus();
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right!';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = '!!!GAME OVER';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Last guess was too low!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Last guess was too high!';
+        }
+    }
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+guessCount++;
+guessField.value = '';
+guessField.focus();
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (const resetPara of resetParas) {
+        resetPara.textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+// Operators / concatenation
+// const myName = 'Bingo';
+// myName;
+// const hello = ' says hello!';
+// hello;
+// const greeting = myName + hello + ' to his new born baby!';
+// greeting;
+
+// /* Augmented assignment operators */
+// let name1 = 'Bingo';
+// name1 += ' says hello!';
+
+// /* The above code is equivalent to */
+// let name2 = 'Bingo';
+// name2 = name2 + ' says hello!';
+
+// Comparison operators
+/* Strict equality (exactly the same) ===
+   Non-equality (not the same) !==
+   Less than <
+   Greater than >
+*/
+
+
+// Conditionals
+
